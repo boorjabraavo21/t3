d85d074dd91cd544acc2867229d441af3abf3979
boorjabraavo21 <115183112+boorjabraavo21@users.noreply.github.com>
2023-05-08 16:39:30 +0200

    Update index.js

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function masInformacion() {
-    alert('Soy Borja Bravo Casermeiro\n1º DAW\nMi proyecto trata sobre');
+    alert('Soy Borja Bravo Casermeiro\n1º DAW\nMi proyecto trata sobre una página que contiene información y '+ 
+    'noticias sobre la música actual, tanto española como internacional'+
+    ', hablando sobre cantantes o grupos y de las canciones o álbumes que van sacando');
 }
 
 function enter(id) {
