40a7049f5fdb8b8eb63eff27258e609a5e59e374
Boorja21 <115183112+boorjabraavo21@users.noreply.github.com>
2023-05-22 20:26:48 +0200

    localStorage aplicado

--- Proyecto/index.js
+++ Proyecto/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 function masInformacion() {
-    alert('Soy Borja Bravo Casermeiro\n1º DAW\nMi proyecto trata sobre una página que contiene información y '+ 
-    'noticias sobre la música actual, tanto española como internacional'+
-    ', hablando sobre cantantes o grupos y de las canciones o álbumes que van sacando');
+    alert('Soy Borja Bravo Casermeiro\n1º DAW\nMi proyecto trata sobre una página que contiene información y ' +
+        'noticias sobre la música actual, tanto española como internacional' +
+        ', hablando sobre cantantes o grupos y de las canciones o álbumes que van sacando');
 }
 
 function enter(id) {
@@ -20,7 +20,7 @@ function aplicarCambios() {
     // mostramos en consola los valores recogidos en la pagina
     var color = document.getElementsByName('colorElegido')[0];
     var bgColor = document.getElementsByName('colorBgElegido')[0];
-    console.log('color elegido '+color.value);
+    console.log('color elegido ' + color.value);
 
     var tam = document.getElementsByName('tamFuente')[0];
     var tamelegido = tam[tam.selectedIndex].value;
@@ -31,17 +31,18 @@ function aplicarCambios() {
     document.documentElement.style.color = color.value;
     document.documentElement.style.backgroundColor = bgColor.value;
     document.getElementById('mensaje').innerHTML = "APLICADO";
-    window.localStorage.setItem("modoGuardado","ajustes");
+
+    // guardamos en localStorage los elementos para aplicarlo en las otras páginas
+    window.localStorage.setItem("tamañoFuente", parseFloat(tamelegido) + "rem");
+    window.localStorage.setItem("colorFuente", color.value);
+    window.localStorage.setItem("colorFondo",bgColor.value);
 }
 
-function leerModoActual() {
-    let modo = window.localStorage.getItem("modoGuardado");
-    console.log(modo);
-    if (!modo) {
-        // devolverá modo DIA si no hay modo guardado
-        modo = "ajustes";
-    } 
-    return modo;
+var tamañoFuente = window.localStorage.getItem("tamañoFuente");
+var colorFuente = window.localStorage.getItem("colorFuente");
+var colorFondo = window.localStorage.getItem("colorFondo");
+if (tamañoFuente && colorFuente && colorFondo) {
+    document.documentElement.style.fontSize = tamañoFuente;
+    document.documentElement.style.color = colorFuente;
+    document.documentElement.style.backgroundColor = colorFondo;
 }
-
-var modo = leerModoActual();
